28d83f3fc9566740ff59dfde88171be978341d5c
Igor de Almeida <igoor1205@gmail.com>
2021-10-23 10:59:11 -0300

    refactor: add generic params to HttpPostClient

--- src/data/test/mock-http-client.ts
+++ src/data/test/mock-http-client.ts
@@ -1,14 +1,14 @@
 import { HttpPostClient, HttpPostParams } from '@/data/protocols/http/http-post-client'
 import { HttpResponse, HttpStatusCode } from '@/data/protocols/http/http-response'
 
-export class HttpPostClientSpy implements HttpPostClient {
+export class HttpPostClientSpy<T, U> implements HttpPostClient<T, U> {
   url?: string;
-  body?: object
-  response: HttpResponse = {
+  body?: T
+  response: HttpResponse<U> = {
     statusCode: HttpStatusCode.ok
   }
 
-  async post (params: HttpPostParams): Promise<HttpResponse> {
+  async post (params: HttpPostParams<T>): Promise<HttpResponse<U>> {
     this.url = params.url
     this.body = params.body
     return Promise.resolve(this.response)
--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -2,17 +2,19 @@ import faker from 'faker'
 import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials-error'
 import { mockAuthentication } from '@/domain/test/mock-authentication'
 import { HttpPostClientSpy } from '@/data/test/mock-http-client'
-import { RemoteAuthentication } from './remote-authentication'
-import { HttpStatusCode } from '@/data/protocols/http/http-response'
 import { UnexpectedError } from '@/domain/errors/unexpected-error'
+import { AuthenticationParams } from '@/domain/usecases/authentication'
+import { AccountModel } from '@/domain/models/account-model'
+import { HttpStatusCode } from '@/data/protocols/http/http-response'
+import { RemoteAuthentication } from './remote-authentication'
 
 type SutTypes = {
   sut: RemoteAuthentication
-  httpPostClientSpy: HttpPostClientSpy
+  httpPostClientSpy: HttpPostClientSpy<AuthenticationParams, AccountModel>
 }
 
 const makeSut = (url = faker.internet.url()): SutTypes => {
-  const httpPostClientSpy = new HttpPostClientSpy()
+  const httpPostClientSpy = new HttpPostClientSpy<AuthenticationParams, AccountModel>()
   const sut = new RemoteAuthentication(url, httpPostClientSpy)
   return {
     sut,
