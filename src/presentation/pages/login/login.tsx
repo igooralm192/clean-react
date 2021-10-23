aaa3fae293192f3b70f6aa62665b9f7a128320c9
Igor de Almeida <igoor1205@gmail.com>
2021-10-23 16:27:26 -0300

    refactor: move input to a separate file

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -5,6 +5,7 @@ import Footer from '@/presentation/components/footer/footer'
 import Spinner from '@/presentation/components/spinner/spinner'
 
 import Styles from './login-styles.scss'
+import Input from '@/presentation/components/input/input'
 
 const Login: React.FC = () => {
   return (
@@ -14,15 +15,8 @@ const Login: React.FC = () => {
       <form className={Styles.form}>
         <h2>Login</h2>
 
-        <div className={Styles.inputWrap}>
-          <input type="email" name="email" placeholder="Digite seu e-mail" />
-          <span className={Styles.status}>🔴</span>
-        </div>
-
-        <div className={Styles.inputWrap}>
-          <input type="password" name="password" placeholder="Digite sua senha" />
-          <span className={Styles.status}>🔴</span>
-        </div>
+        <Input type="email" name="email" placeholder="Digite seu e-mail" />
+        <Input type="password" name="password" placeholder="Digite sua senha" />
 
         <button className={Styles.submit} type="submit">Entrar</button>
 
