659d0ac077477589aa212508a97c7a9193579311
Igor de Almeida <igoor1205@gmail.com>
2021-10-24 12:53:43 -0300

    feat: ensure RequiredFieldValidation returns error if field is empty

---
+++ src/validation/required-field/required-field-validation.ts
@@ -0,0 +1,9 @@
+import { FieldValidation } from '../protocols'
+
+export class RequiredFieldValidation implements FieldValidation {
+  constructor (readonly name: string) {}
+
+  validate (value: string): Error {
+    return value ? null : new Error('Campo obrigatório')
+  }
+}
