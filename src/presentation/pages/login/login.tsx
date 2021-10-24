844db69cbf0642bff8ce1dbefa1272f6c83a8f11
Igor de Almeida <igoor1205@gmail.com>
2021-10-24 10:45:19 -0300

    feat: ensure Login present error and hide spinner on authentication failure

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -25,13 +25,21 @@ const Login: React.FC<Props> = ({ validation, authentication }) => {
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>): Promise<void> => {
     event.preventDefault()
 
-    if (state.isLoading || state.emailError || state.passwordError) {
-      return
+    try {
+      if (state.isLoading || state.emailError || state.passwordError) {
+        return
+      }
+
+      setState({ ...state, isLoading: true })
+
+      await authentication.auth({ email: state.email, password: state.password })
+    } catch (error) {
+      setState({
+        ...state,
+        isLoading: false,
+        mainError: error.message
+      })
     }
-
-    setState({ ...state, isLoading: true })
-
-    await authentication.auth({ email: state.email, password: state.password })
   }
 
   useEffect(() => {
